16ebc9059b038bfc23d6aade1346bf7b5296b716
polinafiksson <pd.fiksson@gmail.com>
2017-06-28 16:03:31 -0500

    added generating of the random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue:Int = 0
+    var targetValue:Int = 0
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        //generating a random Number in the range from 1 to 100
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
     
     @IBAction func showAlert() {
         
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
         let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!",
+                                      message: message,
                                       preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default,
                                    handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        //use the function lroundf() to round the decimal number to the nearest whole
+        //number
+        currentValue = lroundf(sender.value)
+    }
+    
 
 
 }
