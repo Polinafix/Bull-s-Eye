43b88ab0df3202e419ee71bd3132a8fd6eeb8435
polinafiksson <pd.fiksson@gmail.com>
2017-06-28 21:08:47 -0500

    added difference calculaton

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,23 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var targetLabel: UILabel!
     var currentValue:Int = 0
     var targetValue:Int = 0
     @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
+        updateLabels()
+       
+    }
+    
+    func startNewRound(){
+        currentValue = 50
+        slider.value = Float(currentValue)
         //generating a random Number in the range from 1 to 100
         targetValue = 1 + Int(arc4random_uniform(100))
+        
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
 
    
     
     @IBAction func showAlert() {
-        
+        let difference = abs(currentValue - targetValue)
         let message = "The value of the slider is: \(currentValue)" +
-        "\nThe target value is: \(targetValue)"
+        "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
@@ -35,6 +48,8 @@ class ViewController: UIViewController {
                                    handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
